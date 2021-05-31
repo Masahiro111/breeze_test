f15d01a8fe9a9174d6383db8ab1c3f3c55497709
Masahiro111 <33553834+Masahiro111@users.noreply.github.com>
2021-06-01 00:39:21 +0900

    Revert "p15"
    This reverts commit 35eadf2ae254af84e6b9647dd7c0cef312b92bd8.

--- resources/views/books.blade.php
+++ resources/views/books.blade.php
@@ -1,16 +1,8 @@
-<x-testapp>
+<x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
             {{ __('Books') }}
         </h2>
-    </x-slot>
-
-    <x-slot name="title">
-            Laravel Components
-    </x-slot>
-
-    <x-slot name="sidebar">
-        <p>サイドバーに追加できます。</p>
     </x-slot>
 
     <div class="py-12">
@@ -22,4 +14,4 @@
             </div>
         </div>
     </div>
-</x-testapp>
+</x-app-layout>
--- resources/views/components/alert.blade.php
+++
@@ -1,4 +0,0 @@
-<div class="flex justify-between p-4 items-center bg-red-500 text-white">
-    <div>{{ $message }}</div>
-    <button>x</button>
-</div>
