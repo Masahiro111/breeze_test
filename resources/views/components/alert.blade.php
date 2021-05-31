aac795f4e6ea6724288ea771bfddcf15be22332a
Masahiro111 <33553834+Masahiro111@users.noreply.github.com>
2021-05-31 18:52:46 +0900

    p28

--- resources/views/components/alert.blade.php
+++ resources/views/components/alert.blade.php
@@ -1,36 +1,4 @@
-@props([
-    'message' => 'このメッセージは「alert.blade.php」の設定です',
-    'type' => 'success'
-])
-
-@php
-    function addClass($type){
-        $result = '';
-        switch($type){
-            case 'success':
-            $result = 'bg-green-500';
-            break;
-
-            case 'info':
-            $result = 'bg-blue-500';
-            break;
-
-            case 'alert':
-            $result = 'bg-red-500';
-            break;
-            
-            case 'warning':
-            $result = 'bg-yellow-500';
-            break;
-            
-            defalut:
-            $result = 'bg-green-500';
-        }
-        return $result;
-    }
-@endphp
-
-<div {{ $attributes->merge(['class'=>"flex justify-between p-4 items-center text-white " . addClass($type) ]) }}>
-    <div>{{ $message }}</div>
+<div class="flex justify-between p-4 items-center bg-red-500 text-white">
+    <div>これはアラートメッセージです。by alert.blade.php</div>
     <button>x</button>
 </div>
